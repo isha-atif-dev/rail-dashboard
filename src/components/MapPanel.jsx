7c6716e109d75ae8c736f780b7c31d2b4f0d939c
isha-atif-dev <isha.atif02@gmail.com>
2026-07-16 14:31:37 +0100

    Add accurate tube line geometry and tappable disruption reports

--- src/components/MapPanel.jsx
+++ src/components/MapPanel.jsx
@@ -141,8 +141,8 @@ export default function MapPanel({ layers, nearby, activeLines = [], highlightLi
       <MapContainer center={CENTER} zoom={16} zoomControl={false} className="w-full h-full">
         <MapBridge mapRef={mapRef} />
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; Esri &mdash; Esri, HERE, Garmin, FAO, NOAA, USGS'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}"
         />
 
         {Object.entries(tubeLineGeometry)
